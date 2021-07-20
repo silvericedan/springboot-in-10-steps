5eee078b899934af6a0fbe58bcd9a17a38897dc9
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-20 17:53:17 -0300

    printing all the beans that come with spring web

--- src/main/java/com/silvericedan/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/silvericedan/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,19 @@ package com.silvericedan.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		for(String name : applicationContext.getBeanDefinitionNames()){
+			//print all beans currently loaded
+			//System.out.println(name);
+		}
 	}
+
 
 }
